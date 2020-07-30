2bb44925e0b401a5f2fc392be1901665afc8df35
k-o-d-e-n <koden.u8800@gmail.com>
2020-07-30 11:49:50 +0300

    Replaced SwiftAtomics with SE0282 proposal implementation.

--- Package.swift
+++ Package.swift
@@ -1,15 +1,10 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Realtime",
-//    platforms: [
-//        .iOS(.v9), .macOS(.v10_10)
-//    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Realtime",
             targets: ["Realtime"]
@@ -24,17 +19,14 @@ let package = Package(
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(name: "SwiftAtomics", url: "https://github.com/glessard/swift-atomics.git", from: "5.0.1"),
+        .package(url: "https://github.com/apple/swift-se-0282-experimental.git", .branch("master")),
         .package(name: "Promise.swift", url: "https://github.com/k-o-d-e-n/promise.swift.git", .branch("master"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Realtime",
             dependencies: [
-                .product(name: "CAtomics", package: "SwiftAtomics"),
+                .product(name: "SE0282_Experimental", package: "swift-se-0282-experimental"),
                 .product(name: "Promise.swift", package: "Promise.swift")
             ]
         ),
